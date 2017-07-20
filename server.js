fd137d7a74d2ee38a0c821cd3045c736c4a1ac4a
Houssam <houssam.b@hotmail.com>
2017-07-20 08:15:11 -0400

    adding route middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var express 	= require("express");
 var app 		= express();
 var bodyParser 	= require("body-parser");
+var Bear = require('./models/Bear');
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
@@ -14,8 +15,14 @@ var port = process.env.PORT || 8080;
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to the database
 
-var router = express.Router();
+// ROUTES FOR THE API
+var router = express.Router(); // get an instance of the express Router
 
+// Route Middleware
+route.use(function(req, res, next){
+	console.log('Something is happening');
+	next();
+}); 
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
@@ -24,9 +31,11 @@ router.get('/', function(req, res) {
 });
 
 
+// REGISTER THE ROUTES
 app.use('/api', router);
 
-app.listen(port);
 
+// START THE SERVER
+app.listen(port);
 console.log('magic happens on port ' + port);
 
